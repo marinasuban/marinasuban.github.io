83b8ceef98e523e1f90f12590ca1a357325c4859
marina.s <marinasuban1000@yahoo.com>
2021-09-17 17:56:40 +1200

    shelf

--- email.php
+++ email.php
@@ -11,7 +11,9 @@ $headers = "From: $email_from \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header('Location: index.html');
-exit();
+print_r($to);
+print_r($email_subject);
+print_r($email_body);
+print_r($headers);
 
 
